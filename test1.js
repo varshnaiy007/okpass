8a673b9640d1f803aaa128a2c02dee1a06f79280
Varshnaiy <77457051+varshnaiy007@users.noreply.github.com>
2023-08-18 23:05:54 +0530

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,15 @@
+<html>
+        <head>
+                <title>Hello js</title>
+        </head>
+        <body>
+                <h1> I am Heading </h1>
+                <p> I am first paragraph </p>
+
+                <script type="text/javascript">
+                        document.write("Hello world");
+                </script>
+
+                <script src="\test1.js"> </script>
+        </body>
+</html>
